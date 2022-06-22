725b2142ae36eb1976df19a00f1f771000062060
abalkibekov <a.balkibekov@gmail.com>
2022-06-22 11:07:01 +0400

    feat: add recursive function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let nStr = '' + Math.abs(n);
+    let resultStr = '';
+    for (let i = nStr.length - 1; i >= 0; i--) {
+        resultStr += nStr[i];
+    }
+    return +resultStr;
 }
